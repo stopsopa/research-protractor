9ebe2b2c95c4765ccf0eed59e1c3c3c9c3f04522
Szymon Dzialowski <sd@phase-ii.com>
2017-06-01 15:33:20 +0100

    sudo

--- test/angular/angular-page.spec.js
+++ test/angular/angular-page.spec.js
@@ -1,5 +1,6 @@
 describe('angularjs homepage todo list', function() {
-    it('should add a todo', function() {
+
+    it('should add a todo test', function() {
 
         browser.angular(true);
 
@@ -11,10 +12,10 @@ describe('angularjs homepage todo list', function() {
         var todoList = element.all(by.repeater('todo in todoList.todos'));
         expect(todoList.count()).toEqual(3);
         expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-        // You wrote your first test, cross it off the list
+        //
+        // // You wrote your first test, cross it off the list
         todoList.get(2).element(by.css('input')).click();
-
+        //
         var completedAmount = element.all(by.css('.done-true'));
         expect(completedAmount.count()).toEqual(2);
     });
