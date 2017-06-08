4c18c18322682c21d62b8986912e6fb4bcceec20
Szymon Dzialowski <sd@phase-ii.com>
2017-06-08 10:58:54 +0100

    validation of seleniumAddress

--- params/config.php
+++ params/config.php
@@ -6,7 +6,8 @@ echo <<<EOF
     "parameters": {
         "protocol": "http",
         "host": "localhost",
-        "port": 1025
+        "port": 1025,
+        "seleniumAddress" : "http://localhost:4444/wd/hub"
     }
 }
 EOF;
