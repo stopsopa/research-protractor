6cadcb059f6568bb359e1aa3f8a430d59a81abbf
Szymon Dzialowski <sd@phase-ii.com>
2017-06-18 23:01:53 +0100

    readme

--- mac/startup.sh
+++ mac/startup.sh
@@ -2,10 +2,10 @@
 
 cd /Users/admin/Workspace/sel/mac
 
-VBoxManage startvm "vagrant-windows-edge"
+VBoxManage startvm "vagrant-windows-selenium"
 
 sleep 70 && /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://123.123.123.123:4444/grid/console?refresh=10 &
 
-/bin/bash start-selenium.sh 
+/bin/bash start-selenium.sh
 
 
