59250f1b726f6adfe1d94b09d68b1fac5c40cfe3
Szymon Dzialowski <sd@phase-ii.com>
2017-06-19 14:39:33 +0100

    up

---
+++ tests/nong.js
@@ -0,0 +1,22 @@
+/**
+ * http://www.protractortest.org/#/tutorial#step-2-writing-multiple-scenarios
+ */
+describe('Protractor Demo App', function() {
+
+    it('no angular', function () {
+
+        browser.angular(true);
+
+        browser.get('https://stopsopa.github.io/research-protractor/e2e/nong.html');
+
+        browser.find('#go');
+
+        browser.sleep(2000);
+
+        $('#go').click();
+
+        browser.sleep(2000);
+
+        expect($('div').getText()).toEqual("clicked");
+    });
+});
