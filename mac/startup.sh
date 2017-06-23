043d9ca18560eb5f7c00e8e1846638dbeb9f8c19
Simon <stopsopa@users.noreply.github.com>
2017-06-23 09:31:24 +0100

    Update startup.sh

--- mac/startup.sh
+++ mac/startup.sh
@@ -2,6 +2,8 @@
 
 cd /Users/admin/Workspace/sel/mac
 
+/bin/bash vnc-tunnel.sh
+
 VBoxManage startvm "vagrant-windows-selenium"
 
 sleep 70 && /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://123.123.123.123:4444/grid/console?refresh=10 &
