900ff872ea2e5625be98a35b6096045a6280d3b6
Simon <stopsopa@users.noreply.github.com>
2017-06-24 01:01:17 +0100

    Update start-selenium.sh

--- mac/start-selenium.sh
+++ mac/start-selenium.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-/bin/bash start-tunnel.sh
+/bin/bash start-tunnel.sh 
+
+/bin/bash vnc-tunnel.sh & disown
+
+kill -9 $(ps aux | grep -v grep | grep selenium-server | awk '{print $2}') &> /dev/null
+
 java -jar ../selenium-server-standalone-3.4.0.jar -role node -port 5560 -host 127.0.0.1 -hub http://123.123.123.123:4444/grid/register -browser "browserName=chrome, maxInstances=10, platform=SIERRA" -browser "browserName=firefox, maxInstances=10, platform=SIERRA" -browser "browserName=safari, maxInstances=10, platform=SIERRA"
-# https://seleniumhq.github.io/selenium/docs/api/java/org/openqa/selenium/Platform.html
