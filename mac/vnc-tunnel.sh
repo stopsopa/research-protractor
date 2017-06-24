b67b9e4dae19686b2e7340d239ae50600283bd7e
Simon <stopsopa@users.noreply.github.com>
2017-06-24 22:51:57 +0100

    Update vnc-tunnel.sh

--- mac/vnc-tunnel.sh
+++ mac/vnc-tunnel.sh
@@ -5,26 +5,27 @@ chmod 600 ../ssh/id_rsa
 
 PORT=5900
 IP="123.123.123.123"
-
+# 5900 is reserved by vnc https://support.apple.com/en-gb/HT202944
 kill -9 $(ps aux | grep -v grep | grep ssh | grep $PORT | grep "\-R " | awk '{print $2}') &> /dev/null
 
 if [ "$1" != "stop" ]; then
 
-	# while true
-	while :
-	do
-	    TEST=$(ps aux | grep -v grep | grep ssh | grep $PORT && echo 'true' || echo 'false');
+    # while true
+    while :
+    do
+        # TEST=$(ps aux | grep -v grep | grep ssh | grep $PORT && echo 'true' || echo 'false');
 
-	    if [ "$TEST" == "false" ]; then
-	    	echo starting vnc forward
-		date +"%Y-%m-%d %H:%M:%S"
-		ssh root@$IP -N -R 0.0.0.0:$PORT:localhost:$PORT -i ../ssh/id_rsa & disown
-	    fi
+        # if [ "$TEST" == "false" ]; then
+        # fi
 
-	    sleep 2
-	done
+        kill -9 $(ps aux | grep -v grep | grep ssh | grep $PORT | grep "\-R " | awk '{print $2}') &> /dev/null
+        ssh root@$IP -N -R 0.0.0.0:$PORT:localhost:$PORT -i ../ssh/id_rsa & disown
+        echo 'done';
+        sleep 15
+    done	
+
+
 fi
 
-# 5900 is reserved by vnc https://support.apple.com/en-gb/HT202944
 # add also GatewayPorts yes to /etc/ssh/sshd_config
 # https://superuser.com/a/588773
