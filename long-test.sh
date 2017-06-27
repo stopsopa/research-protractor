0cbbba498ef7e471b5d38f88cbcea50a6b464676
Szymon Dzialowski <sd@phase-ii.com>
2017-06-27 10:17:42 +0100

    long

--- long-test.sh
+++ long-test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 while :
 do
     date +"%Y-%m-%d %H:%M:%S--------------"
@@ -9,7 +8,12 @@ do
     echo $?
     echo "$OUTPUT" | tail -n 8
     cd ..
-    sleep $((40 + RANDOM % 600)) # from 40 sec to 10 min
+    NEXT=$((40 + RANDOM % 600));
+    echo next time $(bc -l <<< "$NEXT/60")
+    sleep $NEXT # from 40 sec to 10 min
 done
+
+# run
+# /bin/bash long-test.sh | tee log.log
 
 
