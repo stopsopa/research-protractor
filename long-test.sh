7ec83892485d89013bf24d89220d88f19777d2aa
Szymon Dzialowski <sd@phase-ii.com>
2017-06-27 10:22:04 +0100

    long test 2 one after another

--- long-test.sh
+++ long-test.sh
@@ -4,9 +4,15 @@ do
     date +"%Y-%m-%d %H:%M:%S--------------"
     /bin/bash mac/locker.sh
     cd protractor
+
     OUTPUT=$(node protractor.js --specs ../tests/nong.js)
     echo $?
     echo "$OUTPUT" | tail -n 8
+
+    OUTPUT=$(node protractor.js --specs ../tests/nong.js)
+    echo $?
+    echo "$OUTPUT" | tail -n 8
+
     cd ..
     NEXT=$((40 + RANDOM % 600));
     echo next time $(bc -l <<< "$NEXT/60")
