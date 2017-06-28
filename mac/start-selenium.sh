878dc63df73204ec300ec39b683fde3e2045a806
simon <grytysek@gmail.com>
2017-06-28 23:53:10 +0200

    watcher

--- mac/start-selenium.sh
+++ mac/start-selenium.sh
@@ -8,6 +8,10 @@ kill -9 $(ps aux | grep -v grep | grep vnc-tunnel | awk '{print $2}') &> /dev/nu
 
 /bin/bash vnc-tunnel.sh & disown
 
+kill -9 $(ps aux | grep -v grep | grep locker.sh | awk '{print $2}') &> /dev/null
+
+/bin/bash locker.sh watchdog & disown
+
 kill -9 $(ps aux | grep -v grep | grep selenium-server | awk '{print $2}') &> /dev/null
 
 java -jar ../selenium-server-standalone-3.4.0.jar -role node -port 5560 -host 127.0.0.1 -hub http://$HOST:4444/grid/register -browser "browserName=chrome, maxInstances=10, platform=SIERRA" -browser "browserName=firefox, maxInstances=10, platform=SIERRA" -browser "browserName=safari, maxInstances=10, platform=SIERRA"
