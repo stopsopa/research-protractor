af8d4d4d8c0c4ec0d0167a174796acb5b2d5811c
Szymon <sd@phase-ii.com>
2017-08-27 21:57:16 +0100

    multiline bat

--- windows/start-selenium.cmd
+++ windows/start-selenium.cmd
@@ -1,1 +1,9 @@
-java -jar ..\selenium-server-standalone-3.5.2.jar -role node -port 5559 -host 127.0.0.1 -hub http://%1:4444/grid/register -browser "browserName=MicrosoftEdge, maxInstances=10, platform=WIN10" -browser "browserName=chrome, maxInstances=10, platform=WIN10" -browser "browserName=firefox, maxInstances=10, platform=WIN10" -browser "browserName=internet explorer, version=11, maxInstances=10, platform=WIN10"
+java -jar ..\selenium-server-standalone-3.5.2.jar ^
+-role node ^
+-port 5559 ^
+-host 127.0.0.1 ^
+-hub http://%1:4444/grid/register ^
+-browser "browserName=MicrosoftEdge, maxInstances=10, platform=WIN10" ^
+-browser "browserName=chrome, maxInstances=10, platform=WIN10" ^
+-browser "browserName=firefox, maxInstances=10, platform=WIN10" ^
+-browser "browserName=internet explorer, version=11, maxInstances=10, platform=WIN10"
