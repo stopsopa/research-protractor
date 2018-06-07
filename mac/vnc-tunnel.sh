9404ba1fea4855c86b3c69dfd2bca042a16d2777
Szymon Dzialowski <sd@phase-ii.com>
2018-06-07 09:43:28 +0100

    no message

--- mac/vnc-tunnel.sh
+++ mac/vnc-tunnel.sh
@@ -23,8 +23,13 @@ if [ "$1" != "stop" ]; then
         # fi
 
         kill -9 $(ps aux | grep -v grep | grep ssh | grep $MACVNCPORT | grep "\-R " | awk '{print $2}') &> /dev/null
-        ssh root@$HOST -N -R 0.0.0.0:$MACVNCPORT:localhost:$MACVNCPORT -i ../ssh/id_rsa & disown
+        echo "ssh root@$HOST -N -R 0.0.0.0:$MACVNCPORT:localhost:$MACVNCPORT -i ../ssh/id_rsa & disown"
+        #ssh root@$HOST -N -R 0.0.0.0:$MACVNCPORT:localhost:$MACVNCPORT -i ../ssh/id_rsa & disown
+        echo "autossh -M 0 -f -o \"ServerAliveInterval 30\" -o \"ServerAliveCountMax 3\" -N -R 0.0.0.0:$MACVNCPORT:localhost:$MACVNCPORT root@$HOST -i ../ssh/id_rsa & disown"
+        autossh -M 0 -f -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -R 0.0.0.0:$MACVNCPORT:localhost:$MACVNCPORT root@$HOST -vv -i ../ssh/id_rsa & disown
+
         echo 'done';
+
         sleep $((60 * 30))
     done	
 
